ae3297dc2d0f5efb5e77ffcf8712bf397bfdbd06
Phil Piwonka <wnka@users.noreply.github.com>
2023-07-12 14:59:04 -0700

    clean up linux home

--- modules/home-manager/linux-home/default.nix
+++ modules/home-manager/linux-home/default.nix
@@ -8,18 +8,8 @@
         source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
       end
       fish_add_path /run/current-system/sw/bin
-      fish_add_path /Applications/Emacs.app/Contents/MacOS/
-      fish_add_path /opt/homebrew/bin
-      fish_add_path /Users/piwonka/bin
-      fish_add_path /Users/piwonka/.toolbox/bin
     '';
     shellAliases = {
-      e = "open -a /Applications/Emacs.app";
-      vgm = "pushd ~/syncme/vgm; ./go.sh; popd";
-      pick = "pushd ~/syncme/vgm; ./pick.sh; popd";
-      funtime = "bash ~/bin/funtime.sh";
-      worktime = "bash ~/bin/worktime.sh";
-      nixs = "darwin-rebuild switch --flake ~/code/nix/piwonka-flakes#mac";
     };
   };
 }
