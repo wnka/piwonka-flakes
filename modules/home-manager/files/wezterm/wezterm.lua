1c80c66a3fac7baad563b1a758cbfa1491ccea82
Phil Piwonka <wnka@users.noreply.github.com>
2024-11-12 20:54:40 -0800

    wezterm - hide tab bar if only one tab
    I have no idea why I didn't have this enabled.

--- modules/home-manager/files/wezterm/wezterm.lua
+++ modules/home-manager/files/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font({ family = 'Berkeley Mono' })
 config.font_size = 16.0
 config.line_height = 1.1
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
 config.window_padding = {
