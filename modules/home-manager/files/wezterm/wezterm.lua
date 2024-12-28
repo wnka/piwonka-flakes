000a222eadcd3b22ec25d4bf503b9c1dcc8ed6aa
Phil Piwonka <wnka@users.noreply.github.com>
2024-12-28 11:14:26 -0800

    decrease font size
    my vision isn't THAT bad

--- modules/home-manager/files/wezterm/wezterm.lua
+++ modules/home-manager/files/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font({ family = 'Berkeley Mono' })
-config.font_size = 16.0
+config.font_size = 14.0
 config.line_height = 1.1
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
