d9129c7e938b353214b0a48fee29554e4b534f3e
Phil Piwonka <wnka@users.noreply.github.com>
2025-02-05 23:16:21 +0000

    add envImprovement to path at work

--- modules/home-manager/linux-work/default.nix
+++ modules/home-manager/linux-work/default.nix
@@ -22,6 +22,7 @@
       fish_add_path /nix/var/nix/profiles/default/bin
       fish_add_path /apollo/env/LambdaOperationalTools/bin
       fish_add_path /apollo/env/LambdaStatsOperationalTools/bin
+      fish_add_path /apollo/env/envImprovement/bin
     '';
     shellAliases = {
       emacs-daemon = "TERM=xterm-emacs-leg command emacs --daemon";
