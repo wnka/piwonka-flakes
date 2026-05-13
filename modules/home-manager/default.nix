6ba32d5bdbe9b556638b5657935ebde689681c0b
Phil Piwonka <piwonka@amazon.com>
2026-05-13 23:49:27 +0000

    zz alias for zoxide list

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -157,6 +157,7 @@
       jz = "zellij";
       yy = "yazi";
       hh = "hx";
+      zz = "zi";
       curll = "curl -o /dev/null -w \"lookup:        %{time_namelookup}\nconnect:       %{time_connect}\nappconnect:    %{time_appconnect}\npretransfer:   %{time_pretransfer}\nredirect:      %{time_redirect}\nstarttransfer: %{time_starttransfer}\ntotal:         %{time_total}\n\"";
     };
   };
